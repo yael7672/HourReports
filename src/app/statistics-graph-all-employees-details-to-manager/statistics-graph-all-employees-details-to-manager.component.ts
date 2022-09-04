34bc84d65bbdf91222ec05810340a6680202a979
unknown <yael7672@gmail.com>
2022-09-04 14:45:43 +0300

    update

--- src/app/statistics-graph-all-employees-details-to-manager/statistics-graph-all-employees-details-to-manager.component.ts
+++ src/app/statistics-graph-all-employees-details-to-manager/statistics-graph-all-employees-details-to-manager.component.ts
@@ -13,37 +13,56 @@ import { BarElement, BarController, CategoryScale, Decimation, Filler, Legend, T
 })
 export class StatisticsGraphAllEmployeesDetailsToManagerComponent implements OnInit {
 
-  AllEmployeeChart:any
-  DailyAndMonthlyWorkingHours:any;
-  systemGuid:any
-  constructor(public route: Router, private appService: AppService, private popUpService: PopUpServiceService,private userService:UserServiceService) { }
+  AllEmployeeChartEmployeeDetailsMonthlyWorkingHours: any
+  DailyAndMonthlyWorkingHours: any;
+  systemGuid: any
+  employeeDetails: any;
+  employeeDetailsMonthlyWorkingHours: any
+  employeeDetailsName: any
+  dateChart: any
+  AllEmployeeOpenTasksChart: any
+  employeeDetailsOpenTask: any
+  openChartMonthlyWorkingHours = true;
+  openChartOpenTask = false;
+  constructor(public route: Router, private appService: AppService, private popUpService: PopUpServiceService, private userService: UserServiceService) { }
 
   ngOnInit(): void {
     this.systemGuid = localStorage.getItem('systemGuid');
-    // let personalDetailsCompo = new PersonalDetailsComponent(this.route,this.appService,this.popUpService,this.userService  )
-    // this.DailyAndMonthlyWorkingHours = personalDetailsCompo.GetDailyWorkingHoursAndMonthlyWorkingHours(this.systemGuid)
-    this.GetDailyWorkingHoursAndMonthlyWorkingHours(this.systemGuid)
-  
-    this.AllEmployeeChart = new Chart('BarChart', {
+    this.AllEmployeeChartEmployeeDetailsMonthlyWorkingHours = new Chart('BarChart', {
       type: 'bar',
       data: {
         labels: [],
         datasets: [{
-          label: 'שעות בפועל',
-          data: [0],
+          label: 'שעות עבודה חודשיות',
+          data: [],
           backgroundColor: [
-            'green',
-            'red',
+            'blue',
+            
           ],
           borderColor: [
-            'green',
-            'red',
+            'blue',
+           
           ],
           borderWidth: 1
         },
-        {
-          label: 'שעות עבודה',
-          data: [0],
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+
+    });
+    this.AllEmployeeOpenTasksChart = new Chart('BarChart2', {
+      type: 'bar',
+      data: {
+        labels: [],
+        datasets: [{
+          label: 'משימות פתוחות',
+          data: [],
           backgroundColor: [
             'green',
             'red',
@@ -63,23 +82,58 @@ export class StatisticsGraphAllEmployeesDetailsToManagerComponent implements OnI
           }
         }
       }
-
     });
+    this.GetEmployeeDetails(this.systemGuid)
   }
-  GetDailyWorkingHoursAndMonthlyWorkingHours(systemGuid:any) {
-    this.userService.GetDailyWorkingHoursAndMonthlyWorkingHours(systemGuid).subscribe(
-      res => {
-        if (res) {
-          this.DailyAndMonthlyWorkingHours = res;
-        //  alert( this.DailyAndMonthlyWorkingHours.MonthlyWorkingHours);
-        }
-      }, err => {
+
+  GetEmployeeDetails(systemGuid: any) {
+    this.userService.GetEmployeeDetails(systemGuid).subscribe(
+      (res: any) => {
+        this.employeeDetails = res;
+        this.employeeDetailsMonthlyWorkingHours = this.employeeDetails.map((Employee: any) => Employee.EmployeeMonthlyWorkingHours)
+        this.employeeDetailsName = this.employeeDetails.map((Employee: any) => Employee.EmployeeName)
+        this.employeeDetailsOpenTask = this.employeeDetails.map((Employee: any) => Employee.EmployeeOpenTasks)
+        this.ComparisonEmployeeDetailsOpenTasks()
+        this.ComparisonEmployeeDetailsMonthlyWorkingHours()
+
+      },
+      (err: any) =>
         console.log(err.error)
-      }
     )
   }
-  // ComparisonDailyWorkingHoursAndMonthlyWorkingHours() {
-  //   this.LineChart.data.datasets[0].data =  2;  
-      // this.LineChart.data.datasets[1].data =  this.DailyAndMonthlyWorkingHours.DailyWorkingHours
-  // }
+  ComparisonEmployeeDetailsMonthlyWorkingHours() {
+    this.employeeDetailsMonthlyWorkingHours.forEach((element: any, index: any) => {
+      this.AllEmployeeChartEmployeeDetailsMonthlyWorkingHours.data.datasets[0].data[index] = this.employeeDetailsMonthlyWorkingHours[index];
+    });
+    this.employeeDetailsName.forEach((element: any, index: any) => {
+      this.AllEmployeeChartEmployeeDetailsMonthlyWorkingHours.data.labels[index] = this.employeeDetailsName[index];
+    });
+    this.AllEmployeeChartEmployeeDetailsMonthlyWorkingHours.update();
+  }
+
+  ComparisonEmployeeDetailsOpenTasks() {
+    this.employeeDetailsOpenTask.forEach((element: any, index: any) => {
+      this.AllEmployeeOpenTasksChart.data.datasets[0].data[index] = this.employeeDetailsOpenTask[index];
+    });
+    this.employeeDetailsName.forEach((element: any, index: any) => {
+      this.AllEmployeeOpenTasksChart.data.labels[index] = this.employeeDetailsName[index];
+    });
+    this.AllEmployeeOpenTasksChart.update();
+  }
+  GetMyChart(val: any) {
+    if (val == 2) {
+      this.GetEmployeeDetails(this.systemGuid)
+      // this.openChartMonthlyWorkingHours = false
+      this.openChartOpenTask = true
+
+    }
+    else {
+      if (val == 1) {
+        this.GetEmployeeDetails(this.systemGuid)
+        // this.openChartOpenTask = false
+        this.openChartMonthlyWorkingHours = true
+
+      }
+    }
+  }
 }
