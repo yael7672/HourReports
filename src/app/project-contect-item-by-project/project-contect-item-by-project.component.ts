0afa6f703ccaf6ef3c22ac9e95fac94f563e549b
unknown <yael7672@gmail.com>
2022-10-25 17:08:17 +0300

    update

--- src/app/project-contect-item-by-project/project-contect-item-by-project.component.ts
+++ src/app/project-contect-item-by-project/project-contect-item-by-project.component.ts
@@ -93,7 +93,8 @@ export class ProjectContectItemByProjectComponent implements OnInit {
 
   ExportDataProjectContectItemToExcel() {
     let element = document.getElementById('excel-table'); 
-    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    element?.style.backgroundColor == 'pink'
+     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
