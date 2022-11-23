7f4443e59ff38ab88e603bc8605fd5370d041b91
Adi Revivo <Adilupo181@gmail.com>
2022-11-23 14:02:46 +0200

    updaye

--- src/app/project-contect-items/freedom-project-content-item/freedom-project-content-item.component.ts
+++ src/app/project-contect-items/freedom-project-content-item/freedom-project-content-item.component.ts
@@ -36,14 +36,16 @@ export class FreedomProjectContentItemComponent implements OnInit {
   massgeUserBody = "קיימים דיווחים על התאריכים הנבחרים"
   massgeUserFooter = "?האם ברצונך להמשיך";
   isDisabled = false;
-  thArrTableProjectContentItem = ['שם', 'תאריך' ];
+  thArrTableProjectContentItem = ['שם', 'תאריך'];
   projectContentItemListKeys = ['Name', 'Date'];
   projectContentItemToCreate: any;
   ifShowSpinner!: boolean;
   constructor(private userService: UserServiceService, private appService: AppService, private popUpService: PopUpServiceService,
-    private datepipe: DatePipe) {this.appService.getSpinner().subscribe(res => {
+    private datepipe: DatePipe) {
+    this.appService.getSpinner().subscribe(res => {
       this.ifShowSpinner = res;
-    }) }
+    })
+  }
 
   ngOnInit(): void {
   }
@@ -60,22 +62,39 @@ export class FreedomProjectContentItemComponent implements OnInit {
   }
   CreateNewFreedomProjectItem(form: NgForm) {
     this.appService.setSpinner(true);
+    if (this.isChecked) {
+      if (this.fromDate && this.untilDate != null && this.fromDate && this.untilDate != "") {
+        let d1 = new Date(this.fromDate);
+        let d2 = new Date(this.untilDate)
+        if (d1.getTime() <= d2.getTime()) {
+          this.fromDate = this.datepipe.transform( this.fromDate, 'dd/MM/yyyy')
+          this.untilDate = this.datepipe.transform(this.untilDate, 'dd/MM/yyyy')
+        }
+        else {
+          swal('!תאריך התחלה לא יכול להיות גדול מתאריך סיום');
+          this.appService.setSpinner(false);
+          return;
+        }
+      }
+      else {
+        swal('!עליך להזין תאריך התחלה ותאריך סיום')
+        this.appService.setSpinner(false);
+        return;
+      }
+    }
+    else {
+      this.fromDate = this.datepipe.transform(form.value.oneDate, 'dd/MM/yyyy')
+      this.untilDate = this.datepipe.transform(form.value.oneDate, 'dd/MM/yyyy')
+    }
     this.isDisabled = true;
     form.value.Name = "יום חופש";
     if (!form.value.ActualTime)
-      form.value.ActualTime = "9"
+    form.value.ActualTime = "9"
     form.value.WorkType = { "Guid": "00EE906B-6ADD-EA11-A813-000D3A21015B" }
     form.value.Project = { "Guid": "216003B0-9D6B-EC11-8943-000D3A38C560" }
     form.value.BillableHours = "2";
     form.value.OwnerId = { "Guid": localStorage.getItem('systemGuid') }
-    if (this.isChecked) {
-      this.fromDate = this.datepipe.transform(form.value.fromDate, 'dd/MM/yyyy')
-      this.untilDate = this.datepipe.transform(form.value.untilDate, 'dd/MM/yyyy')
-    } else {
-      this.fromDate = this.datepipe.transform(form.value.oneDate, 'dd/MM/yyyy')
-      this.untilDate = this.datepipe.transform(form.value.oneDate, 'dd/MM/yyyy')
-    }
-    form.value.MoreEmployee=[]
+    form.value.MoreEmployee = []
     this.projectContentItemToCreate = form.value;
     this.checkIfIsReportOnThisDate()
   }
@@ -85,15 +104,15 @@ export class FreedomProjectContentItemComponent implements OnInit {
       (res) => {
         this.appService.setSpinner(false);
         this.massage = res;
-        swal(this.massage)
+        this.fromDate ="";
+        this.untilDate ="";
+        swal(this.massage);
         this.popUpService.setAllmyProjectContectItem(true)
         this.popUpService.SetWorkTimeAfterProjectContectItem(true)
         this.appService.setIsPopUpOpen(false);
         this.popUpService.setClosePopUp();
       },
-      (err) =>
-      
-      {
+      (err) => {
         this.appService.setSpinner(false);
         swal(err.error)
         this.isDisabled = false;
@@ -109,17 +128,20 @@ export class FreedomProjectContentItemComponent implements OnInit {
     if (kindOfMassage = 'checkIfIsReportOnThisDate') {
       this.showMassgeToUser = false;
       this.isDisabled = false;
+      this.fromDate ="";
+      this.untilDate ="";
 
     }
   }
   checkIfIsReportOnThisDate() {
-    this.systemGuid = localStorage.getItem('systemGuid')
-    this.userService.GetMyProjectContectItem(this.systemGuid, 5, this.fromDate, this.untilDate,"").subscribe(res => {
+    this.systemGuid = localStorage.getItem('systemGuid');
+    debugger
+    this.userService.GetMyProjectContectItem(this.systemGuid, 5, this.fromDate, this.untilDate, "").subscribe(res => {
       if (res) {
         this.appService.setSpinner(false);
         this.MyProjectContectItemArr = res;
         console.log(this.MyProjectContectItemArr);
-        
+
         if (this.MyProjectContectItemArr.length > 0) {
           this.showMassgeToUser = true;
         }
